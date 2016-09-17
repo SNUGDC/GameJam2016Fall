db9730053f205afd0c55ec51a4864c7622bc3455
yulcat <alszmtm@gmail.com>
2016-09-17 23:30:39 +0900

    벽에 그림자 오브젝트 추가

---
+++ Assets/Scripts/SpriteShadow.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// [ExecuteInEditMode]
+public class SpriteShadow : MonoBehaviour {
+	public bool isMovingObject = false;
+	void OnDrawGizmos()
+	{
+		MoveShadowToPosition();
+	}
+	void Update()
+	{
+		if(isMovingObject)
+		{
+			MoveShadowToPosition();
+		}
+	}
+	void MoveShadowToPosition()
+	{
+		transform.position = transform.parent.position + new Vector3(-1,-1,1);
+	}
+}
