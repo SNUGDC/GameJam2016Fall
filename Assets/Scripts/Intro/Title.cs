26a13325f3a835e3d2725b15ae2ce79ed591aa76
Juhyeong <majecty@gmail.com>
2016-09-18 10:06:41 +0900

    Show title bling bling

---
+++ Assets/Scripts/Intro/Title.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+//using DG.Tweening.ShortcutExtensions;
+
+public class Title : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		transform.DOShakeScale (2, 0.05f, 2, 20, false).SetEase (Ease.InOutSine).SetLoops (-1);
+	}
+}
